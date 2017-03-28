7cf01ebd9f32470f0bd07269294fde0111020c6b
Juho Mäkinen <juho.makinen@applifier.com>
2017-03-28 09:16:59 +0300

    Support correct merging of feature_manifests

--- lib/biosphere/state.rb
+++ lib/biosphere/state.rb
@@ -2,6 +2,7 @@ require 'pp'
 require 'ipaddress'
 require 'biosphere/node'
 require 'deep_merge'
+require 'deep_dup'
 
 class Biosphere
     class State
@@ -51,7 +52,34 @@ class Biosphere
 
         def load_from_structure!(structure)
             if @node
-                @node.data.deep_merge(structure.data)
+
+                # Objects which might get removed when building a new state need to be stored away before the merge
+                feature_manifests = {}
+                @node.data[:deployments].each do |name, deployment|
+                    if deployment[:feature_manifests]
+                        feature_manifests[name] = ::DeepDup.deep_dup(deployment[:feature_manifests])
+                    end
+                end
+
+                # Merge the structured state into the current state
+                @node.data.deep_merge(structure.data, {:overwrite_arrays => true})
+
+                # Now re-apply the stored objects on top of the merged state so that removes are handled correctly
+                @node.data[:deployments].each do |name, deployment|
+                    removed = {}
+                    if feature_manifests[name]
+                        feature_manifests[name].each do |section, manifests|
+                            diff = deployment[:feature_manifests][section] - manifests
+                            removed[section] = diff if diff.length > 0
+                        end
+                        deployment[:feature_manifests] = feature_manifests[name]
+                    end
+
+                    if removed.length > 0
+                        deployment[:removed_feature_manifests] = removed
+                    end
+                end
+
             else
                 @node = structure
             end
--- spec/biosphere/state_spec.rb
+++ spec/biosphere/state_spec.rb
@@ -30,56 +30,139 @@ RSpec.describe Biosphere::State do
         expect(s.node[:foo]).to eq("bar")
     end
 
-    it "can merge loaded state into a nested state tree" do
+    describe "load_from_structure!" do
+        it "can merge loaded state into a nested state tree" do
 
-        s = Biosphere::State.new
-        s.node[:root] = "root"
-        s.node[:deployments] = {
-            "foo" => {
-            },
-            "bar" => {
-            }
-        }
-
-        foo = s.node[:deployments]["foo"]
-        bar = s.node[:deployments]["bar"]
-
-        structure = Class.new {
-            attr_accessor :data
-        }.new
-        structure.data = {
-            root: "root",
-            deployments: {
+            s = Biosphere::State.new
+            s.node[:root] = "root"
+            s.node[:deployments] = {
                 "foo" => {
-                    name: "foo",
-                    certs: {
-                        machine: "cert 1 here"
+                },
+                "bar" => {
+                }
+            }
+
+            foo = s.node[:deployments]["foo"]
+            bar = s.node[:deployments]["bar"]
+
+            structure = Class.new {
+                attr_accessor :data
+            }.new
+            structure.data = {
+                root: "root",
+                deployments: {
+                    "foo" => {
+                        name: "foo",
+                        certs: {
+                            machine: "cert 1 here"
+                        }
+                    },
+                    "bar" => {
+                        name: "bar",
+                        certs: {
+                            machine: "cert 2 here"
+                        }
+                    }
+                },
+                nested: {
+                    very: {
+                        wow: "yeah"
+                    }
+                }
+            }
+
+            s.load_from_structure!(structure)
+
+            expect(foo[:name]).to eq("foo")
+            expect(bar[:name]).to eq("bar")
+
+            expect(foo[:certs][:machine]).to eq("cert 1 here")
+            expect(bar[:certs][:machine]).to eq("cert 2 here")
+
+            expect(s.node[:nested][:very][:wow]).to eq("yeah")
+
+        end
+
+        it "can remove feature manifests on merge" do
+            s = Biosphere::State.new
+            s.node[:root] = "root"
+
+            # This is the state which the new build created.
+            # We'll simulate a situation where there was more than one feature_manifests under "foo" .. "test"
+            # but then that gets removed
+            s.node[:deployments] = {
+                "foo" => {
+                    feature_manifests: {
+                        test: [
+                            "foo"
+                        ]
                     }
                 },
                 "bar" => {
-                    name: "bar",
-                    certs: {
-                        machine: "cert 2 here"
+                    feature_manifests: {
+                        test: [
+                            "foo",
+                            "bar"
+                        ]
+                    }
+                },
+                "baz" => {
+                    feature_manifests: {
+                        test: [
+                            "foo"
+                        ]
+                    }
+                }                
+            }
+
+            foo = s.node[:deployments]["foo"]
+            bar = s.node[:deployments]["bar"]
+
+            structure = Class.new {
+                attr_accessor :data
+            }.new
+
+            # This represents the stored state where the "foo" .. "test" still had more than one feature_manifest
+            structure.data = {
+                deployments: {
+                    "foo" => {
+                        feature_manifests: {
+                            test: [
+                                "foo",
+                                "bar"
+                            ]
+                        }
+                    },
+                    "bar" => {
+                        feature_manifests: {
+                            test: [
+                                "foo"
+                            ]
+                        }
+                    },
+                    "baz" => {
+                        feature_manifests: {
+                            test: [
+                                "foo"
+                            ]
+                        }
                     }
                 }
-            },
-            nested: {
-                very: {
-                    wow: "yeah"
-                }
             }
-        }
 
-        s.load_from_structure!(structure)
+            s.load_from_structure!(structure)
 
-        expect(foo[:name]).to eq("foo")
-        expect(bar[:name]).to eq("bar")
+            # Make sure the feature manifest is removed
+            expect(s.node[:deployments]["foo"][:feature_manifests][:test]).to eq(["foo"])
 
-        expect(foo[:certs][:machine]).to eq("cert 1 here")
-        expect(bar[:certs][:machine]).to eq("cert 2 here")
+            # And placed into the removed list
+            expect(s.node[:deployments]["foo"][:removed_feature_manifests][:test]).to eq(["bar"])
 
-        expect(s.node[:nested][:very][:wow]).to eq("yeah")
+            # This should be the same
+            expect(s.node[:deployments]["bar"][:feature_manifests][:test]).to eq(["foo", "bar"])
+            expect(s.node[:deployments]["baz"][:feature_manifests][:test]).to eq(["foo"])
 
+        end
     end
     
 end
